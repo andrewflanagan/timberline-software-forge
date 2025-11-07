9651a3ffb52bdf1cc5bcdd30e2903068a85a45cc
Andrew Flanagan <andrew.flanagan@gmail.com>
2025-11-06 19:36:18 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,8 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://example.com",
+	output: "server",
+	site: "https://timberlinecodeforge.com",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
